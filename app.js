b54ee64ed8594b0110941baf7760bc40af573bbd
Eashwar Mohan <eashwar@umich.edu>
2019-08-18 15:54:51 -0700

    forgot to close parentheses

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const db = require('./db.js');
 const app = express();
 const port = process.env.PORT || 8000;
 
-app.get('/test', (req, res) => res.send('Hello! This is the FAMNM Backend.'))
+app.get('/test', (req, res) => res.send('Hello! This is the FAMNM Backend.'));
 app.get('/meeting_types', async (req, res) => {
 	try {
 		const { rows } = await db.meeting_types();
@@ -13,7 +13,7 @@ app.get('/meeting_types', async (req, res) => {
 	} catch (e) {
 		res.status(500).send({error: 'something went wrong.'});
 	}
-}
+});
 
 
 app.listen(port, () => console.log(`app listening at https:\/\/localhost:${port}!`))
