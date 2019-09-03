b7086a3c946884ec6f96d7b6fd5cc9ef3917d29b
eashwar <eashwar@umich.edu>
2019-09-03 19:24:25 -0400

    CommonJS Module Syntax

--- db/meeting_types.js
+++ db/meeting_types.js
@@ -1,3 +1,5 @@
-export const create_meeting_type = "INSERT INTO MEETING TYPE (TYPE) VALUES $1";
-export const get_all_meeting_types = "SELECT TYPE FROM MEETING_TYPE";
-export const delete_meeting_type = "DELETE FROM MEETING TYPE WHERE TYPE = $1";
+module.exports = {
+    create_meeting_type: "INSERT INTO MEETING TYPE (TYPE) VALUES $1",
+    get_all_meeting_types: "SELECT TYPE FROM MEETING_TYPE",
+    delete_meeting_type: "DELETE FROM MEETING TYPE WHERE TYPE = $1"
+};
