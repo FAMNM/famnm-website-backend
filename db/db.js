4723f245840d90ea5f9579dee44efa176ac76b07
eashwar <eashwar@umich.edu>
2019-09-03 19:27:47 -0400

    reference explicitly module.exports query function

--- db/db.js
+++ db/db.js
@@ -18,27 +18,27 @@ module.exports = {
 			})
 	},
 	get_all_meeting_types: () => {
-		return this.query({text: meeting_types.get_all_meeting_types});
+		return module.exports.query({text: meeting_types.get_all_meeting_types});
 	},
 	create_meeting: (new_meeting) => {
-		return this.query({text: meetings.create_meeting, values: new_meeting});
+		return module.exports.query({text: meetings.create_meeting, values: new_meeting});
 	},
 	get_all_meetings: () => {
-		return this.query({text: meetings.get_all_meetings});
+		return module.exports.query({text: meetings.get_all_meetings});
 	},
 	get_meetings_on_day: (day) => {
-		return this.query({text: meetings.get_meetings_on_day, values: [day]});
+		return module.exports.query({text: meetings.get_meetings_on_day, values: [day]});
 	},
 	get_meetings_within_day_range: (days) => {
-		return this.query({text: meetings.get_meetings_within_day_range, values: days});
+		return module.exports.query({text: meetings.get_meetings_within_day_range, values: days});
 	},
 	get_meetings_of_type: (type) => {
-		return this.query({text: meetings.get_meetings_of_type, values: [type]});
+		return module.exports.query({text: meetings.get_meetings_of_type, values: [type]});
 	},
 	update_meeting: (new_info) => {
-		return this.query({text: meetings.update_meeting, values: new_info});
+		return module.exports.query({text: meetings.update_meeting, values: new_info});
 	},
 	delete_meeting: (id) => {
-		return this.query({text: meetings.delete_meeting, values: [id]});
+		return module.exports.query({text: meetings.delete_meeting, values: [id]});
 	},
 }
