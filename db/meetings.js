d2b72dedf317e6fe56a0eedc5df268254b19b180
Eashwar Mohan <luigi629@gmail.com>
2019-09-08 16:39:29 -0400

    semicolons!

--- db/meetings.js
+++ db/meetings.js
@@ -1,9 +1,9 @@
 module.exports = {
     create_meeting: 'INSERT INTO MEETING (TYPE, DAY, START_TIME, END_TIME, DESCRIPTION) VALUES ($1, $2, $3, $4, $5);',
     get_all_meetings: 'SELECT * FROM MEETING;',
-    get_meetings_on_day: 'SELECT * FROM MEETING WHERE DAY = $1',
+    get_meetings_on_day: 'SELECT * FROM MEETING WHERE DAY = $1;',
     get_meetings_within_day_range: 'SELECT * FROM MEETING WHERE DAY GE $1 AND DAY LE $2;',
     get_meetings_of_type: 'SELECT * FROM MEETING WHERE TYPE = $1;',
-    update_meeting: 'UPDATE MEETING SET TYPE = $1, DAY = $2, START_TIME = $3, END_TIME = $4, DESCRIPTION = $5 WHERE MEETING_ID = $6',
+    update_meeting: 'UPDATE MEETING SET TYPE = $1, DAY = $2, START_TIME = $3, END_TIME = $4, DESCRIPTION = $5 WHERE MEETING_ID = $6;',
     delete_meeting: 'DELETE FROM MEETING WHERE MEETING_ID = $1;',
 };
