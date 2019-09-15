5f01c6c443603f7020f2d0556aad23fe7c908ef8
Eashwar Mohan <eashwar@umich.edu>
2019-09-15 17:50:16 -0400

    fix syntax for date range query

--- db/meetings.js
+++ db/meetings.js
@@ -3,7 +3,7 @@ module.exports = {
     get_meeting_with_id: 'SELECT * FROM MEETING WHERE MEETING_ID = $1;',
     get_all_meetings: 'SELECT * FROM MEETING;',
     get_meetings_on_day: 'SELECT * FROM MEETING WHERE DAY = $1;',
-    get_meetings_within_day_range: 'SELECT * FROM MEETING WHERE DAY GE $1 AND DAY LE $2;',
+    get_meetings_within_day_range: 'SELECT * FROM MEETING WHERE DAY BETWEEN $1 AND $2;',
     get_meetings_of_type: 'SELECT * FROM MEETING WHERE TYPE = $1;',
     update_meeting: 'UPDATE MEETING SET TYPE = $1, DAY = $2, START_TIME = $3, END_TIME = $4, DESCRIPTION = $5 WHERE MEETING_ID = $6;',
     delete_meeting: 'DELETE FROM MEETING WHERE MEETING_ID = $1 RETURNING MEETING_ID;',
