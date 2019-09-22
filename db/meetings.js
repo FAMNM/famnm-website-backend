c66a55e9b34e8d93dab388f6c43c6f8d70cb6a95
Eashwar Mohan <eashwar@umich.edu>
2019-09-22 16:22:31 -0400

    add RETURNING to UPDATE

--- db/meetings.js
+++ db/meetings.js
@@ -5,6 +5,6 @@ module.exports = {
     get_meetings_on_day: 'SELECT * FROM MEETING WHERE DAY = $1;',
     get_meetings_within_day_range: 'SELECT * FROM MEETING WHERE DAY BETWEEN $1 AND $2;',
     get_meetings_of_type: 'SELECT * FROM MEETING WHERE TYPE = $1;',
-    update_meeting: 'UPDATE MEETING SET TYPE = $1, DAY = $2, START_TIME = $3, END_TIME = $4, DESCRIPTION = $5 WHERE MEETING_ID = $6;',
+    update_meeting: 'UPDATE MEETING SET TYPE = $1, DAY = $2, START_TIME = $3, END_TIME = $4, DESCRIPTION = $5 WHERE MEETING_ID = $6 RETURNING MEETING_ID;',
     delete_meeting: 'DELETE FROM MEETING WHERE MEETING_ID = $1 RETURNING MEETING_ID;',
 };
