43ba2a944dfe2dfef26e86756ad2be8f65a0b092
yottalogical <email@yottalogical.com>
2021-11-08 19:56:10 -0500

    Implement the GET /meeting/validate endpoint

--- initialize.sql
+++ initialize.sql
@@ -2,28 +2,25 @@ CREATE TABLE meeting_types (
     meeting_type TEXT PRIMARY KEY
 );
 
-CREATE TABLE uniqnames (
+CREATE TABLE members (
     uniqname TEXT PRIMARY KEY,
     mentor BOOLEAN
 );
 
 CREATE TABLE meetings (
     meeting_id SERIAL PRIMARY KEY,
-    meeting_type TEXT NOT NULL REFERENCES meeting_types (meeting_type) ON DELETE CASCADE,
+    meeting_type TEXT NOT NULL REFERENCES meeting_types(meeting_type) ON DELETE CASCADE,
     meeting_date DATE NOT NULL,
     UNIQUE (meeting_type, meeting_date)
 );
 
 CREATE TABLE attendance (
-    uniqname TEXT NOT NULL REFERENCES uniqnames (uniqname) ON DELETE CASCADE,
-    meeting_id INTEGER NOT NULL REFERENCES meetings (meeting_id) ON DELETE CASCADE,
+    uniqname TEXT NOT NULL REFERENCES members(uniqname) ON DELETE CASCADE,
+    meeting_id INTEGER NOT NULL REFERENCES meetings(meeting_id) ON DELETE CASCADE,
     PRIMARY KEY (uniqname, meeting_id)
 );
 
 CREATE TABLE semesters (
-    starting_date DATE NOT NULL,
-    ending_date DATE NOT NULL,
+    starting_date DATE PRIMARY KEY,
     semester_name TEXT NOT NULL,
-    PRIMARY KEY (starting_date, ending_date),
-    CHECK (starting_date < ending_date)
 );
--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 from utilities import *
 
@@ -15,12 +15,50 @@ def meeting_types():
 
 @app.route('/meeting/validate')
 def validate_meeting():
-    return 'Hello, World!'
+    meeting = request.get_json()
+    id = request.args.get('id')
 
+    with db_connection() as conn:
+        # Check if meeting already exists
+        with conn.cursor() as cur:
+            if id is None:
+                cur.execute(
+                    'SELECT COUNT(*) '
+                    'FROM meetings '
+                    'WHERE meeting_type = %s '
+                    'AND meeting_date = %s',
+                    (meeting['meeting_type'], meeting['meeting_date'])
+                )
+            else:
+                cur.execute(
+                    'SELECT COUNT(*) '
+                    'FROM meetings '
+                    'WHERE meeting_type = %s '
+                    'AND meeting_date = %s '
+                    'AND meeting_id != %s',
+                    (meeting['meeting_type'], meeting['meeting_date'], id)
+                )
+            (identical_meetings,) = cur.fetchone()
 
-@app.route('/meeting/validate/id/<int:id>')
-def validate_meeting_by_id(id):
-    return 'Hello, World!'
+        # Check for unrecognized uniqnames
+        new_uniqnames = list()
+        for uniqname in meeting['attendees']:
+            with conn.cursor() as cur:
+                cur.execute(
+                    'SELECT COUNT(*) '
+                    'FROM members '
+                    'WHERE uniqname = %s',
+                    (uniqname,)
+                )
+                (attendee_exists,) = cur.fetchone()
+            
+            if attendee_exists == 0:
+                new_uniqnames.append(uniqname)
+
+    return {
+        'already_exists': identical_meetings > 0,
+        'new_uniqnames': new_uniqnames
+    }
 
 
 @app.route('/meeting', methods=['POST'])
